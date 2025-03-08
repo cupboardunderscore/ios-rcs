a430c72ae593c01a7978b21ffbbbc17c9f33f0de
cup <54553422+cupboardunderscore@users.noreply.github.com>
2025-03-08 17:22:44 +0100

    more icons testing

--- buildHtml.tsx
+++ buildHtml.tsx
@@ -49,13 +49,16 @@ const CarrierSupportTable = () => {
                 {
                     carr = null;
                 }
-                let carrname:string = data.names[0];
-                let carr2:string = carrname + "." + data.countryCode;
-                if (carr2.includes(" "))
-                {
-                    carr2 = null;
-                }
-                let url = data.data.CarrierBookmarks?.at(-1)?.URL || data.data.MyAccountURL || data.data.TetheringURL || carr || carr2;
+                carr = carr.replace(".US", ".com");
+                carr = carr.replace(".UK", ".co.uk");
+                carr = carr.replace(".AU", ".com.au");             
+                //let carrname:string = data.names[0];
+                //let carr2:string = carrname + "." + data.countryCode;
+                //if (carr2.includes(" "))
+                //{
+                //    carr2 = null;
+                //}
+                let url = data.data.CarrierBookmarks?.at(-1)?.URL || data.data.MyAccountURL || data.data.TetheringURL || carr;// || carr2;
                 return <div class='carrier' data-supports={rcsStatus(data)}>
                     <div class='header'>
                         

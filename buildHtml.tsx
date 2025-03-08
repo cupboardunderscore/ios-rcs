2b511f90983e6bb62d213f5e3c68acb8b1e14333
cup <54553422+cupboardunderscore@users.noreply.github.com>
2025-03-08 16:03:33 +0100

    added ios version info

--- buildHtml.tsx
+++ buildHtml.tsx
@@ -91,7 +91,7 @@ let html = renderToString(<>
                     <> </>&bull; <> </>
                     <a href='https://github.com/cupboardunderscore/ios-rcs'>GitHub</a>
                 </p>
-                <h2>Last updated: {new Date().toDateString()}</h2>
+                <h2>Updated with iOS 18.4 beta 2 carrier bundles!</h2>
             </header>
             <CarrierSupportTable />
         </div>

c6982afa54156cd33173a907cfed1981bc6982ea
cupboard_ <54553422+cupboardunderscore@users.noreply.github.com>
2025-03-08 15:49:29 +0100

    added worldwide

--- buildHtml.tsx
+++ buildHtml.tsx
@@ -39,7 +39,7 @@ const CarrierSupportTable = () => {
         (aCarriers?.filter(([id, data]) => data.data.RCS).length ?? 0) 
     );
     
-    return <div class='countries'>{entries.map(([country, carriers]) => (country !== "🌐 Worldwide" && <>
+    return <div class='countries'>{entries.map(([country, carriers]) => (country !== "🌐-Worldwide" && <>
         <h2>{country}</h2>
         <div class='carriers'>
             {carriers?.map(([id, data]) => {

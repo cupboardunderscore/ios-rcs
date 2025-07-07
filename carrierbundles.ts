0d2022e55e6a6527227c1779b4787a4bbdc4e1dc
cup <54553422+cupboardunderscore@users.noreply.github.com>
2025-07-07 19:49:54 +0200

    26b3

--- carrierbundles.ts
+++ carrierbundles.ts
@@ -218,13 +218,13 @@ async function doOnline() {
 
 export function manualversion()
 {
-    return "18.6 beta 2 and 26.0 beta 2";
+    return "18.6 beta 2 and 26.0 beta 3";
 }
 
 doLocal('18.5-CrystalF22F76.D93OS')
 await doOnline();
 doLocal('18.6b2-CrystalGSeed22G5064d.D93DeveloperOS')
-doLocal('26.0b2-LuckSeed23A5276f.D93DeveloperOS')
+doLocal('26.0b3-LuckSeed23A5287g.D93DeveloperOS')
 
 fs.writeFileSync(Path.join(__dirname, 'version.txt'), manualversion());
 fs.writeFileSync(Path.join(__dirname, 'processed.json'), JSON.stringify(networks, null, 2));

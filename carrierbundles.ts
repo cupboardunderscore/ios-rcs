f3ac4f802ddefce2897ae15aacb397905251c35f
cup <killcupboard@icloud.com>
2025-10-20 20:21:13 +0200

    26.1b4

--- carrierbundles.ts
+++ carrierbundles.ts
@@ -233,12 +233,12 @@ async function doOnline() {
 
 export function manualversion()
 {
-    return "26.0.1 and 26.1 beta 3";
+    return "26.0.1 and 26.1 beta 4";
 }
 
 doLocal('26.0.1-Luck23A355.D93OS')
 await doOnline();
-doLocal('26.1b3-LuckBSeed23B5064e.D93DeveloperOS')
+doLocal('26.1b4-LuckBSeed23B5073a.D93DeveloperOS')
 
 fs.writeFileSync(Path.join(__dirname, 'version.txt'), manualversion());
 fs.writeFileSync(Path.join(__dirname, 'processed.json'), JSON.stringify(networks, null, 2));

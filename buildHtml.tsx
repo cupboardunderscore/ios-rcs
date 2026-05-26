bf7f47ffaac3b2b5695496b496f47dad35f7cb2e
cupboard_ <killcupboard@icloud.com>
2026-05-26 20:06:49 +0200

    code cleanup

--- buildHtml.tsx
+++ buildHtml.tsx
@@ -105,7 +105,7 @@ export function build(type: number, carr: string, dir: string, tag: string, ptag
 
     let carriers = eval(carr) as Record<string, { source: string, version: string, names: string[], country?: string, countryCode: string, data: CarrierPlist, blob: CarrierPlist }>;
 
-    let rcsStatus = (data: typeof carriers[string], id: string) => eval(tag) ? ((data.source.includes("DeveloperOS") || type == 2) ? /*((+data.version.slice(0, 4) >= 64.5) ? 1 : 2)*/ 2 : data.source.startsWith("https") ? 3 : 4) : 0;
+    let rcsStatus = (data: typeof carriers[string], id: string) => eval(tag) ? ((data.source.includes("DeveloperOS") || type == 2) ? 1 : data.source.startsWith("https") ? 2 : 3) : 0;
 
     let count: number = 0;
     let ccount: number = 0;
@@ -154,11 +154,11 @@ export function build(type: number, carr: string, dir: string, tag: string, ptag
                                 {url && <img width={23} height={23} src={`https://www.google.com/s2/favicons?domain=${encodeURIComponent(url)}&sz=32`} alt={data.names[0]}/>}
                                 <a target={'_blank'} rel={'noopener noreferrer'} href={site} style={'color:var(--grey-900); text-decoration:none;'}>{data.names[0]}</a>
                             </h3>
-                            <span class='emoji'>{['❌','⏳','⏳','✅','✅'][rcsStatus(data, id)]}</span>
+                            <span class='emoji'>{['❌','⏳','✅','✅'][rcsStatus(data, id)]}</span>
                         </div>
                         {data.names.length > 1 && <p class='aka'>aka. {data.names.slice(1).join(", ")}</p>}
                         {eval(tag) && (
-                            (data.source.includes("DeveloperOS") || type == 2) ? /*((+data.version.slice(0, 4) >= 64.5) ? "in beta (iOS 26)" : "in beta")*/ "in beta" :
+                            (data.source.includes("DeveloperOS") || type == 2) ? "in beta" :
                             data.source.startsWith("https") ? <a target="_blank" href="https://support.apple.com/en-us/109324">delivered OTA</a> : "")}
                         <div class='grow'></div>
                         <p class='id'>{id} {data.version}</p>

9689c91aefd7eae096487dcdea3a05ff29db7673
cupboard_ <killcupboard@icloud.com>
2026-09-16 19:43:36 +0200

    27.2b1

--- carrierbundles.ts
+++ carrierbundles.ts
@@ -366,12 +366,12 @@ async function doOnline() {
 
 export function manualversion()
 {
-    return "27.0";
+    return "27.0 and 27.2 beta 1";
 }
 
 doLocal('27.0-Rave24A437.V159OS')
 await doOnline();
-//doLocal('')
+doLocal('27.2b1-RaveSeedUpdate24B5084k.V159DeveloperOS')
 
 fs.writeFileSync(Path.join(__dirname, 'version.txt'), manualversion());
 fs.writeFileSync(Path.join(__dirname, 'processed.json'), JSON.stringify(networks, null, 2));
